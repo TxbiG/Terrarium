c26626eeabee93488db04bc400dffe208ce28514
Toby <126118957+TxbiG@users.noreply.github.com>
2026-08-23 13:26:55 +0100

    Change uint32 to uint32_t in ext4.h

--- kernel/fs/ext4/ext4.h
+++ kernel/fs/ext4/ext4.h
@@ -16,15 +16,15 @@ extern "C" {
 #define TERRA_EXT4_FEATURE_RO_COMPAT_METADATA_CSUM 0x00000400u
 
 typedef struct terra_ext4_volume {
-    uint32 block_device;
-    uint32 inode_count;
-    uint64 block_count;
-    uint32 block_size;
-    uint32 blocks_per_group;
-    uint32 inodes_per_group;
-    uint32 feature_compat;
-    uint32 feature_incompat;
-    uint32 feature_ro_compat;
+    uint32_t block_device;
+    uint32_t inode_count;
+    uint64_t block_count;
+    uint32_t block_size;
+    uint32_t blocks_per_group;
+    uint32_t inodes_per_group;
+    uint32_t feature_compat;
+    uint32_t feature_incompat;
+    uint32_t feature_ro_compat;
     uint16 inode_size;
     uint16 state;
     int has_extents;
@@ -32,8 +32,8 @@ typedef struct terra_ext4_volume {
     int readonly;
 } terra_ext4_volume_t;
 
-int terra_ext4_probe(uint32 block_device);
-int terra_ext4_mount(uint32 block_device, terra_ext4_volume_t *out_volume);
+int terra_ext4_probe(uint32_t block_device);
+int terra_ext4_mount(uint32_t block_device, terra_ext4_volume_t *out_volume);
 int terra_ext4_describe(const terra_ext4_volume_t *volume, char *buffer, size_t size);
 const char *terra_ext4_status(void);
 
