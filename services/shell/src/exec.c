c3a1d7174d7543a630f9447aa084b6ccdb9961f8
Toby <126118957+TxbiG@users.noreply.github.com>
2026-08-23 15:05:37 +0100

    Implement Windows command execution in exec.c
    Added Windows compatibility for command execution using _spawnvp.

--- services/shell/src/exec.c
+++ services/shell/src/exec.c
@@ -1,5 +1,9 @@
+#ifdef _WIN32
+#include <process.h>
+#else
 #include <unistd.h>
 #include <sys/wait.h>
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <shell/exec.h>
@@ -9,26 +13,28 @@
 int shell_exec(char **argv) {
     if (!argv[0])
         return 0;
-
     int argc = 0;
     while (argv[argc])
         argc++;
-
     if (strcmp(argv[0], "cd") == 0)
         return shell_builtin_cd(argc, argv);
-
     if (strcmp(argv[0], "exit") == 0)
         return shell_builtin_exit(argc, argv);
-
     if (strcmp(argv[0], "pwd") == 0)
         return shell_builtin_pwd(argc, argv);
-
     if (strcmp(argv[0], "help") == 0)
         return shell_builtin_help(argc, argv);
-
     if (strcmp(argv[0], "source") == 0 || strcmp(argv[0], ".") == 0)
         return shell_builtin_source(argc, argv);
 
+#ifdef _WIN32
+    intptr_t status = _spawnvp(_P_WAIT, argv[0], (const char *const *)argv);
+    if (status == -1) {
+        perror(argv[0]);
+        return 1;
+    }
+    return (int)status;
+#else
     pid_t pid = fork();
     if (pid == 0) {
         execvp(argv[0], argv);
@@ -45,6 +51,6 @@ int shell_exec(char **argv) {
         perror("fork");
         return 1;
     }
-
     return 0;
+#endif
 }
