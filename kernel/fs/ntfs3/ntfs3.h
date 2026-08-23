9e78afd3a985f55394b6226109fdc948cbdddbf4
Toby <126118957+TxbiG@users.noreply.github.com>
2026-08-23 13:26:21 +0100

    Change uint32 to uint32_t in ntfs3.h

--- kernel/fs/ntfs3/ntfs3.h
+++ kernel/fs/ntfs3/ntfs3.h
@@ -8,21 +8,21 @@ extern "C" {
 #endif
 
 typedef struct terra_ntfs3_volume {
-    uint32 block_device;
-    uint32 bytes_per_sector;
-    uint32 sectors_per_cluster;
-    uint32 cluster_size;
-    uint64 total_sectors;
-    uint64 mft_lcn;
-    uint64 mft_mirror_lcn;
-    uint32 file_record_size;
-    uint32 index_record_size;
-    uint64 volume_serial;
+    uint32_t block_device;
+    uint32_t bytes_per_sector;
+    uint32_t sectors_per_cluster;
+    uint32_t cluster_size;
+    uint64_t total_sectors;
+    uint64_t mft_lcn;
+    uint64_t mft_mirror_lcn;
+    uint32_t file_record_size;
+    uint32_t index_record_size;
+    uint64_t volume_serial;
     int readonly;
 } terra_ntfs3_volume_t;
 
-int terra_ntfs3_probe(uint32 block_device);
-int terra_ntfs3_mount(uint32 block_device, terra_ntfs3_volume_t *out_volume);
+int terra_ntfs3_probe(uint32_t block_device);
+int terra_ntfs3_mount(uint32_t block_device, terra_ntfs3_volume_t *out_volume);
 int terra_ntfs3_describe(const terra_ntfs3_volume_t *volume, char *buffer, size_t size);
 const char *terra_ntfs3_status(void);
 
