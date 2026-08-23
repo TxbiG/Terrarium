ec0ce634d637fc9c16b6cdb2d35a1cbc35e5119f
Toby <126118957+TxbiG@users.noreply.github.com>
2026-08-23 13:34:29 +0100

    Change uint32 and uint64 to uint32_t and uint64_t

--- kernel/fs/xfs/xfs.h
+++ kernel/fs/xfs/xfs.h
@@ -8,26 +8,26 @@ extern "C" {
 #endif
 
 typedef struct terra_xfs_volume {
-    uint32 block_device;
-    uint32 block_size;
-    uint64 data_blocks;
-    uint64 realtime_blocks;
-    uint64 realtime_extents;
-    uint64 log_start;
-    uint64 root_inode;
-    uint32 realtime_extent_size;
-    uint32 allocation_group_blocks;
-    uint32 allocation_group_count;
-    uint32 log_blocks;
-    uint16 version;
-    uint16 sector_size;
-    uint16 inode_size;
+    uint32_t block_device;
+    uint32_t block_size;
+    uint64_t data_blocks;
+    uint64_t realtime_blocks;
+    uint64_t realtime_extents;
+    uint64_t log_start;
+    uint64_t root_inode;
+    uint32_t realtime_extent_size;
+    uint32_t allocation_group_blocks;
+    uint32_t allocation_group_count;
+    uint32_t log_blocks;
+    uint16_t version;
+    uint16_t sector_size;
+    uint16_t inode_size;
     int crc_enabled;
     int readonly;
 } terra_xfs_volume_t;
 
-int terra_xfs_probe(uint32 block_device);
-int terra_xfs_mount(uint32 block_device, terra_xfs_volume_t *out_volume);
+int terra_xfs_probe(uint32_t block_device);
+int terra_xfs_mount(uint32_t block_device, terra_xfs_volume_t *out_volume);
 int terra_xfs_describe(const terra_xfs_volume_t *volume, char *buffer, size_t size);
 const char *terra_xfs_status(void);
 
